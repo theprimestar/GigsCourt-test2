23a995c520ce7d3eb0ae638edf4a4ebe1f07e04f
Gigscourt <theprimestarventures@gmail.com>
2026-05-06 16:03:14 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/auth_screen.dart';
 
 void main() async {
