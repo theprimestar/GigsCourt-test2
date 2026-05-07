65d6bd1d9eb87a59788299d96a5f6e19613f7449
Gigscourt <theprimestarventures@gmail.com>
2026-05-07 05:46:36 +0100

    Update auth_screen.dart

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../features/auth/data/firebase_auth_repository.dart';
+import '../services/firebase_auth_repository.dart';
 
 class AuthScreen extends StatefulWidget {
   final VoidCallback onAuthenticated;
